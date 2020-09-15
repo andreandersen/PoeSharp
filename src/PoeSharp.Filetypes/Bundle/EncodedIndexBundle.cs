17f183c274c5b943b21b68eb88ba1b83ce15b3bb
Andre Andersen <thezensei@gmail.com>
2020-09-15 18:08:16 +0200

    Fix mixed indenting

--- src/PoeSharp.Filetypes/Bundle/EncodedIndexBundle.cs
+++ src/PoeSharp.Filetypes/Bundle/EncodedIndexBundle.cs
@@ -88,19 +88,19 @@ namespace PoeSharp.Filetypes.Bundle
     uint32 total_payload_size;
     uint32 head_payload_size;
     struct head_payload_t {
-	    enum <uint32> {Kraken_6 = 8, Mermaid_A = 9, Leviathan_C = 13 } first_file_encode;
-	    uint32 unk03;
-	    uint64 uncompressed_size2;
-	    uint64 total_payload_size2;
-	    uint32 entry_count;
-	    uint32 unk28[5];
-	    uint32 entry_sizes[entry_count];
+        enum <uint32> {Kraken_6 = 8, Mermaid_A = 9, Leviathan_C = 13 } first_file_encode;
+        uint32 unk03;
+        uint64 uncompressed_size2;
+        uint64 total_payload_size2;
+        uint32 entry_count;
+        uint32 unk28[5];
+        uint32 entry_sizes[entry_count];
     } head;
 
     local int i <hidden=true>;
     for (i = 0; i < head.entry_count; ++i) {
-	    struct entry_t {
-		    byte data[head.entry_sizes[i]];
-	    } entry;
+        struct entry_t {
+            byte data[head.entry_sizes[i]];
+        } entry;
     }
 */
